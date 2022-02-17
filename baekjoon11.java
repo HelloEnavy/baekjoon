9f8c1e08a0c9caec000d26abdac2c79814f6b432
HelloEnavy <gusdk1793@gmail.com>
2022-02-18 03:02:27 +0900

    0218 baekjoon11.java 추가

--- baekjoon11.java
+++ baekjoon11.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
 
 public class baekjoon11 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        scanner.nextLine();
-        String number = scanner.nextLine();
-        String[] numArr = new String[num];
-        int sum = 0;
 
-        for(int i=0 ; i<num ; i++) {
-            numArr[i] = number.substring(i,i+1);
-            sum+=Integer.parseInt(numArr[i]);
-        }
-        
-        System.out.println(sum);
-        
+        char answer = scanner.nextLine().charAt(0);
+        int number = (int)answer;
+
+        System.out.println(number);
+
     }
 }
