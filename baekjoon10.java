292c57ba3b3c00cbc43a8ebfe493c0b61b8a9ad9
HelloEnavy <gusdk1793@gmail.com>
2022-02-18 02:59:17 +0900

    0218

--- baekjoon10.java
+++ baekjoon10.java
@@ -15,18 +15,20 @@ public class baekjoon10 {
             if(num2<100) {
                 count++;
             } else {
+                //각 자리 수의 차이를 list에 추가
                 while(num2 >= 10){
                     int number = num2%10;
                     num2=num2/10;
                     int number2 = num2%10;
                     list.add(number2-number);
                 }
-
+                //list의 값을 비교해서 같은게 아니면 false로 바뀌게 처리
                 for(int j=0 ; j<list.size()-1 ; j++) {
                     if(list.get(j)!=list.get(j+1)) {
                         check = false;                                                 
                     }
                 }
+                //check가 true상태면(다른게 없다면) count 증가
                 if(check) {
                     count++;
                 }
